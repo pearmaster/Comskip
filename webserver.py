431da1cd377b1f4f476aa4bab6d45e6cd2857039
Jacob Brunson <github@jacobbrunson.com>
2024-10-29 02:33:17 +0000

    Background job

--- webserver.py
+++ webserver.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 from queue import Queue
 import subprocess
+import threading
 
 app = Flask(__name__)
 q = Queue()
 current_output = None
 
-def background_job(arg):
+def background_job():
+    global current_output
     while True:
         path = q.get()
         proc = subprocess.Popen(["/usr/local/bin/comskip", "--ini=/app/comskip.ini", path], stdout=subprocess.PIPE)
@@ -14,14 +16,17 @@ def background_job(arg):
 
 @app.route('/add-job', methods=['POST'])
 def add_job():
+    global q
     data = request.get_json()
-    arg = data.get('path')
-    q.put(arg)
-    return "OK", 200
+    if arg := data.get('path'):
+        q.put(arg)
+        return f"There are {q.qsize()} jobs queued", 200
+    return "Invalid Argument", 401
 
-@app.route('/status', method=['GET'])
+@app.route('/status')
 def get_current():
     def gen():
+        global current_output
         while chunk := current_output.read(128):
             yield chunk
 
@@ -32,4 +37,6 @@ def get_current():
 
 if __name__ == '__main__':
     print("Running on port 5000")
+    thread = threading.Thread(target=background_job)
+    thread.start()
     app.run(host='0.0.0.0', port=5000)
